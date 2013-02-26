2322e3c83710c92f52e9388a91d15cd65c804ee7
Steven Yi <stevenyi@gmail.com>
2013-02-26 17:57:10 +0000

    updated project settings to warn-on-reflection when using leon repl

--- project.clj
+++ project.clj
@@ -4,4 +4,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :warn-on-reflection true
+  ;:main 
+            )
