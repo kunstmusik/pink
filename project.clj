9ae10bf91b1c713bb9e3b0b48f2b8b84477fd3ee
Steven Yi <stevenyi@gmail.com>
2013-03-20 12:57:56 +0000

    experimenting with profiles, updated to clojure 1.5.1

--- project.clj
+++ project.clj
@@ -1,10 +1,14 @@
 (defproject audio-seq "0.1.0-SNAPSHOT"
   :jvm-opts ["-server" "-Xmx2g" "-XX:-UseParallelGC"]
-  :description "FIXME: write description"
+  :description "A library for music research, composition, and performance."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
   :warn-on-reflection true
+  :profiles {
+      :dev {
+          :warn-on-reflection true
+          :dependencies [[criterium "0.3.1"]]}}
   ;:main 
-            )
+)
