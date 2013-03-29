8c796af9c2bfba764929fcce9ba87331046409d5
Steven Yi <stevenyi@gmail.com>
2013-03-29 15:25:35 +0100

    commented out warn-on-reflection in base of project

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
-  :warn-on-reflection true
+;  :warn-on-reflection true
   :profiles {
       :dev {
           :warn-on-reflection true
