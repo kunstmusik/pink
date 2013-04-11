ea40560d464f07cee11578b25f740ad92e807b6b
Steven Yi <stevenyi@gmail.com>
2013-04-11 23:24:55 +0100

    implemented basic audio engine and polyphony handling

--- src/audio_seq/core.clj
+++ src/audio_seq/core.clj
@@ -1,8 +1,8 @@
 (ns audio-seq.core
-  (:require [audio-seq.engine :as engine]
+  (:require [audio-seq.engine :as eng]
             [audio-seq.envelopes :refer [env]]
             [audio-seq.oscillators :refer [sine]]
-            [audio-seq.util :refer [mix mul]]))
+            [audio-seq.util :refer [mix mul const]]))
 
 
     
@@ -13,5 +13,33 @@
         (take x (iterate inc 1))))
     (env [0.0 0.0 0.05 1 0.05 0.9 0.5 0.9 0.5 0])))
 
+
+(defn simple-synth [freq]
+  (mul
+    (mix 
+      (sine freq 0)
+      (mul (const 0.5) (sine (* 2 freq) 0))
+      (mul (const 0.25) (sine (* 3 freq) 0))
+      (mul (const 0.125) (sine (* 4 freq) 0)))
+    (mul
+      (const 0.25)
+      (env [0.0 0.0 0.02 1 0.05 0.9 0.2 0.9 0.2 0]))))
+
 (defn demo3 [x] (engine/run-audio-block (audio-block3 x)))
+
+(defn demo4 []
+  (let [melody (take (* 4 4) (cycle [220 330 440 330]))
+        dur 0.25
+        e (eng/engine-create)]
+    (eng/engine-start e)
+    (loop [[x & xs] melody]
+      (println x)
+      (when x
+        (send e assoc :audio-funcs (conj (@e :audio-funcs) (simple-synth x)))
+        (Thread/sleep (* 1000 dur))
+        (recur xs)))
+    (eng/engine-stop e)))
+
+     
+     
 
--- src/audio_seq/engine.clj
+++ src/audio_seq/engine.clj
@@ -1,36 +1,59 @@
 (ns audio-seq.engine
   "Audio Engine Code"
-  (:import (java.nio ByteBuffer)
-           (javax.sound.sampled AudioFormat AudioSystem
+  (:import 
+    (java.util Arrays)
+    (java.nio ByteBuffer)
+    (javax.sound.sampled AudioFormat AudioSystem
                                 SourceDataLine)))
+
+
+(defn map-d 
+  "Maps function f across double[] buffers and writes output to final passed in buffer" 
+  ([f ^doubles a ^doubles b]
+    (when (and a b)
+      (let [l (alength a)]
+        (loop [cnt 0]
+          (when (< cnt l)
+            (aset b cnt ^double (f (aget a cnt)))
+            (recur (unchecked-inc cnt))))
+        b)))
+  ([f ^doubles a ^doubles b ^doubles c]
+    (when (and a b c)
+      (let [l (alength a)]
+        (loop [cnt 0]
+          (when (< cnt l)
+            (aset c cnt ^double (f (aget a cnt) (aget b cnt)))
+            (recur (unchecked-inc cnt))))
+        c))))
 
 (def af (AudioFormat. 44100 16 1 true true))
 
-(def buffer-size 1024)
-
 (def ^:dynamic *sr* 44100)
 (def ^:dynamic *ksmps* 64)
+
+(def buffer-size 256)
+(def write-buffer-size (/ buffer-size 2))
+(def frames (quot write-buffer-size *ksmps*))
+
+
+
+(defn ^short limit [^double num]
+  (if (> num Short/MAX_VALUE)
+    Short/MAX_VALUE 
+    (if (< num Short/MIN_VALUE)
+      Short/MIN_VALUE 
+      (short num))))
 
 ;;;; Engine
 
 (defn engine-create []
   "Creates an engine"
-  {:status (atom :stopped)
+  (agent {:status :stopped
    :line nil
-   :current-audio-funcs nil})
+   :out-buffer (double-array *ksmps*)
+   :buffer (ByteBuffer/allocate buffer-size)
+   :audio-funcs []}))
 
-(defn engine-start [engine]
-  (when (= @(:status engine) :stopped)
-    (swap! (:status engine) :initializing)
-    (comment do setup, open audio line, start running)
-    ))
-
-(defn engine-stop [engine]
-  (when (not= @(:status engine) :running)
-    (println "Stopping Engine")))
-
-(defn engine-status [engine]
-  @(:status engine))
 
 ;; should do initialization of f on separate thread?
 (defn engine-add-active-func [engine f]
@@ -47,24 +70,85 @@
     (.open audio-format)
     (.start))))
 
+(defn map-over-d [f ^doubles buf]
+  (let [len (alength buf)]
+    (loop [y 0]
+      (when (< y len)
+        (f (aget buf y))
+        (recur (unchecked-inc y))))))
+
+(defn run-audio-funcs [afs ^doubles buffer]
+  (loop [[x & xs] afs ret []]
+    (if x 
+      (let [b (x)]
+        (if b
+          (do 
+            (map-d + buffer b buffer)
+            (recur xs (conj ret x)))
+          (recur xs ret)))
+     ret))) 
+
+(defn process-frame 
+  [afuncs ^doubles outbuffer ^SourceDataLine line ^ByteBuffer buffer frames]
+  (loop [x 0 afs afuncs]
+    (if (< x frames)
+      (do
+        (Arrays/fill ^doubles outbuffer 0.0)
+        (let [newfs (run-audio-funcs afs outbuffer)]
+          (map-over-d #(.putShort buffer (limit (* Short/MAX_VALUE %))) outbuffer)
+          (recur (inc x) newfs)))
+      (do
+        (.write line (.array buffer) 0 buffer-size)
+        (.clear buffer)
+        afs))))
+
+(defn engine-run [engine a]
+  (if (= (engine :status) :running)
+    (let [line (engine :line)
+          outbuf (engine :out-buffer)
+          buf (engine :buffer)
+          afs (process-frame (engine :audio-funcs) outbuf line buf frames)
+          neweng (assoc engine :audio-funcs afs)]
+      (send a engine-run a)
+      neweng)
+    (do
+      (println "stopping...")
+      (doto ^SourceDataLine (engine :line)
+        (.flush)
+        (.close))
+      engine)))
+
+(defn engine-start [engine]
+  (when (= (:status @engine) :stopped)
+    (let [#^SourceDataLine line (open-line af)]
+      (send engine assoc :status :running :line line)
+      (send engine engine-run engine))))
+
+(defn engine-stop [engine]
+  (when (= (:status @engine) :running)
+    (send engine assoc :status :stopped :line nil)))
+
+(defn engine-status [engine]
+  (:status @engine))
+
+
 (defn run-audio-block [a-block]
-  (let [#^SourceDataLine line (open-line af)
-        audio-block a-block]
-    (let [cnt (/ (* *sr* 5.0) buffer-size)
+  (let [#^SourceDataLine line (open-line af) 
         buffer (ByteBuffer/allocate buffer-size)
         write-buffer-size (/ buffer-size 2)
         frames (quot write-buffer-size *ksmps*)]
-      (loop [c cnt] 
-       (when (pos? c) 
-         (loop [x 0]
-           (when (< x frames)
-             (let [buf ^doubles (a-block)]
-               (loop [y 0]
-                 (when (< y (alength buf))
-                   (.putShort buffer (short (* Short/MAX_VALUE (aget buf y))))
-                   (recur (unchecked-inc y)))) 
-               (recur (unchecked-inc x)))))
-         (.write line (.array buffer) 0 buffer-size)
-         (.clear buffer)
-      (recur (dec c) ))))
+    (loop [x 0]
+      (if (< x frames)
+        (if-let [buf ^doubles (a-block)]
+          (do
+            (map-over-d #(.putShort buffer (limit (* Short/MAX_VALUE %))) buf)
+            (recur (unchecked-inc x)))
+          (do
+            (.write line (.array buffer) 0 buffer-size)
+            (.clear buffer)))
+        (do
+          (.write line (.array buffer) 0 buffer-size)
+          (.clear buffer)
+          (recur 0))))
+    (.flush line)
     (.close line)))
