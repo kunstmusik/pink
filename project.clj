b31ded72d6047da00eda258e8e2374b60879a480
Steven Yi <stevenyi@gmail.com>
2013-04-15 18:12:21 +0100

    exerpimenting with overtone.at-at, sample accurate scheduling of notes

--- project.clj
+++ project.clj
@@ -1,14 +1,30 @@
 (defproject audio-seq "0.1.0-SNAPSHOT"
-  :jvm-opts ["-server" "-Xmx2g" "-XX:-UseParallelGC"]
+  :jvm-opts ;["-server" "-Xmx2g" "-XX:-UseParallelGC"]
+   ["-Xms512m" "-Xmx1g"           ; Minimum and maximum sizes of the heap
+    "-XX:+UseParNewGC"            ; Use the new parallel GC in conjunction with
+    "-XX:+UseConcMarkSweepGC"     ;  the concurrent garbage collector
+    "-XX:+CMSConcurrentMTEnabled" ; Enable multi-threaded concurrent gc work (ParNewGC)
+    "-XX:MaxGCPauseMillis=20"     ; Specify a target of 20ms for max gc pauses
+    "-XX:+CMSIncrementalMode"     ; Do many small GC cycles to minimize pauses
+    "-XX:MaxNewSize=257m"         ; Specify the max and min size of the new
+    "-XX:NewSize=256m"            ;  generation to be small
+    "-XX:+UseTLAB"                ; Uses thread-local object allocation blocks. This
+                                  ;  improves concurrency by reducing contention on
+                                  ;  the shared heap lock.
+    "-XX:MaxTenuringThreshold=0"] ; Makes the full NewSize available to every NewGC
   :description "A library for music research, composition, and performance."
+
   :url "http://example.com/FIXME"
+
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-;  :warn-on-reflection true
+
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [overtone/at-at "1.1.1"]]
   :profiles {
-      :dev {
-          :warn-on-reflection true
-          :dependencies [[criterium "0.3.1"]]}}
+             :dev {
+                   :warn-on-reflection true
+                   :dependencies [[criterium "0.3.1"]]}}
+
   ;:main 
-)
+  )
--- src/audio_seq/demo/demo1.clj
+++ src/audio_seq/demo/demo1.clj
@@ -2,7 +2,8 @@
   (:require [audio-seq.engine :as eng]
             [audio-seq.envelopes :refer [env]]
             [audio-seq.oscillators :refer [sine]]
-            [audio-seq.util :refer [mix mul const]]))
+            [audio-seq.util :refer [mix mul const create-buffer getd setd!]])
+  (:use [overtone.at-at]))
 
 
 (defn simple-synth [freq]
@@ -18,25 +19,65 @@
 
 
 (defn demo [e]
-  (let [melody (take (* 4 4) (cycle [220 330 440 330]))
+  (let [melody (take (* 4 8) (cycle [220 330 440 330]))
         dur 0.25]
     (loop [[x & xs] melody]
-      (println x)
       (when x
-        (send e #(assoc % :audio-funcs (conj (% :audio-funcs) (simple-synth x) (simple-synth (* 2 x)))))
-        (Thread/sleep (* 1000 dur))
-        (recur xs)))))
+        (let [afs (e :pending-funcs)]
+          (dosync
+            (alter afs conj (simple-synth x) (simple-synth (* 2 x))))
+        (recur xs))))))
+
+
+(defn demo-afunc [e]
+  (let [melody (ref (take (* 4 8) (cycle [220 330 440 330])))
+        dur 0.25 
+        cur-time (double-array 1 0.0)
+        time-incr (/ eng/*ksmps* 44100.0)
+        afs (e :pending-funcs)
+        out (create-buffer)]
+    (dosync (alter afs conj (simple-synth 440)))
+    (fn ^doubles []
+      (let [t (+ (getd cur-time) time-incr)]
+        (when (> t dur)
+          (dosync (alter afs conj (simple-synth 440))))
+        (setd! cur-time (rem t dur)))
+      out
+      )))
+
 
 ;;
 
 (comment
 
-(let [e (eng/engine-create)]
-  (eng/engine-start e)
-  (demo e)
-  (Thread/sleep 500)
-  (eng/engine-stop e))
-     
-)
+  (defn note-sender[e]
+    (dosync
+     (alter (e :pending-funcs) conj (simple-synth 440) (simple-synth 660))))
+
+(def my-pool (mk-pool))
+
+(def e (eng/engine-create))
+(eng/engine-start e)
+(dosync (alter (e :pending-funcs) conj (demo-afunc e)))
+(eng/engine-stop e)
+
+(every 250 (partial note-sender e) my-pool :fixed-delay true)
+(every 250 (partial note-sender e) my-pool)
+         
+(stop-and-reset-pool! my-pool)
+e
+
+
+  (let [e (eng/engine-create)]
+    (eng/engine-start e)
+    (demo e)
+    (Thread/sleep 500)
+    (eng/engine-stop e))
+
+  (let [e (eng/engine-create)]
+    (eng/engine-start e)
+    (demo e)
+    (Thread/sleep 500)
+    (eng/engine-stop e)))
 
 
