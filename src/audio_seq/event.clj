16dca9b402c9fb5d208e21272a17ab37a5d70fa4
Steven Yi <stevenyi@gmail.com>
2013-07-29 01:42:53 +0200

    continuing work on event lists, experimenting with using sorted-set-by sets

--- src/audio_seq/event.clj
+++ src/audio_seq/event.clj
@@ -8,8 +8,22 @@
    :init-args args
    :start start
    :duration end
+   :state (atom :inactive)
    }
   )
+
+(defn event-comparator [a b]
+  "comparator function for sorted set of events"
+  (if (= a b)
+    0
+    (let [x (compare (:start a) (:start b))]
+      (if (not= 0 x)
+        x 
+        (let [y (compare (:duration a) (:duration b))]
+          (if (not= 0 y)
+            y
+            -1))))))
+
 
 (defn events [f & args]
   (map #(apply event f %) args))
@@ -22,10 +36,53 @@
 ;    (process [this] )
 ;    ))
 
+;; NOTE - wondering if this should be something that implements ISeq or Set interface, as an event list is something like a sorted list of events, with active and inactive being separate views...
+(deftype EventList [evts active inactive curtime]
+  AudioFunc
+  (process [this] nil)
+  )
+
+(defn event-list
+  "Creates an EventList"
+  ([] (event-list []))
+  ([evts] 
+   (EventList.
+     (ref (apply sorted-set-by event-comparator evts)) 
+     (ref #{})
+     (ref (into #{} evts)) 
+     0.0)))
+
+(defn event-list-add [^EventList evtlst evt]
+  "Add and event to an event list"
+  (do
+    (dosync
+      (alter (.evts evtlst) conj evt)
+      (alter (.inactive evtlst) conj evt))
+    evtlst))
+
+(defn event-list-remove [^EventList evtlst evt]
+  "remove an event from the event list"
+  (do
+    (dosync
+      (alter (.evts evtlst) disj evt)
+      (alter (.active evtlst) disj evt)
+      (alter (.inactive evtlst) disj evt)) 
+    evtlst))
 
 (comment
+
 
   (defn test-func [])
   (events test-func [1 2 3] [4 5 6])
 
+(def test-note (event test-func 0.0 1.0 440.0))
+(def test-note2 (event test-func 0.0 1.0 220.0))
+(def test-note3 (event test-func 1.0 1.5 110.0))
+
+(def n (apply sorted-set-by event-comparator [test-note test-note2 test-note3]))
+(= n #{test-note test-note2 test-note3})  
+
+(def evtlst (event-list [test-note]))
+(event-list-add evtlst test-note2)
+(event-list-remove evtlst test-note)
   )
--- test/audio_seq/event_test.clj
+++ test/audio_seq/event_test.clj
@@ -13,3 +13,43 @@
    (is (= [4.0 :test] (:init-args evt))) 
    (is (= test-audio-func (:init-func evt))) 
     ))
+
+(deftest event-list-test
+  (let [test-note (event test-audio-func 0.0 1.0 440.0)
+        evtlst (event-list [test-note])]
+    (is (= #{} @(.active evtlst)))
+    (is (= #{test-note} @(.inactive evtlst)))
+    (is (= #{test-note} @(.evts evtlst))) 
+    ))
+
+(deftest event-list-add-test
+  (let [test-note (event test-audio-func 0.0 1.0 440.0)
+        test-note2 (event test-audio-func 0.0 1.1 880.0)
+        test-note3 (event test-audio-func 0.0 1.0 220.0)
+        evtlst (event-list [test-note2])]
+    (event-list-add evtlst test-note)
+    (is (= #{} @(.active evtlst)))
+    (is (= #{test-note test-note2} @(.inactive evtlst)))
+    (is (= #{test-note test-note2} @(.evts evtlst))) 
+
+    ;;Test that adding same note is skipped
+    (event-list-add evtlst test-note)
+    (is (= 2 (count @(.evts evtlst))))
+
+    (event-list-add evtlst test-note3)
+    (is (= 3 (count @(.evts evtlst))))
+    (is (= #{test-note test-note2 test-note3} @(.evts evtlst))) 
+    ))
+
+(deftest event-list-remove-test
+  (let [test-note (event test-audio-func 0.0 1.0 440.0)
+        test-note2 (event test-audio-func 0.0 1.0 880.0)
+        evtlst (event-list [test-note]) ]
+    (event-list-add evtlst test-note2)
+;    (print "\n OMG " @(.evts evtlst) "\n")
+    (event-list-remove evtlst test-note)
+    (is (= #{} @(.active evtlst)))
+    (is (= #{test-note2} @(.inactive evtlst)))
+    (is (= #{test-note2} @(.evts evtlst))) 
+    ))
+
