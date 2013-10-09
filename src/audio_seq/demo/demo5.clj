7ee784ca7ca3d4dd9e84a409333bc3e0ad6072b1
Steven Yi <stevenyi@gmail.com>
2013-10-09 12:23:27 -0400

    implemented exponential envelope

---
+++ src/audio_seq/demo/demo5.clj
@@ -0,0 +1,72 @@
+;; Test of Exponential Envelope 
+;; index is held in an atom, reader reads from atom and returns a buffer
+
+(ns audio-seq.demo.demo5
+  (:require [audio-seq.engine :as eng]
+            [audio-seq.envelopes :refer [env exp-env]]
+            [audio-seq.oscillators2 :refer [sine sine2]]
+            [audio-seq.util :refer [mix mul swapd! sum const create-buffer getd setd! arg shared let-s reader]]))
+
+
+(defn fm-synth [freq]
+  (let-s [e (env [0.0 0.0 0.05 2 0.02 1.5 0.2 1.5 0.2 0])] 
+    (mul
+        (sine2 (sum freq (mul e 440 (sine freq))))
+        (mul 0.4 e))))
+
+;; test design work
+;; mutable value will be held in an atom
+;; reader will be the audio-func to read from the atom
+
+(def index (atom 1))
+(def t (reader index))
+(reset! index 3.25)
+
+(aget (t) 0) 
+
+(defn fm-bell [freq]
+  (let-s [e (exp-env [0.0 0.00001 0.05 1.0 3 0.000001])] 
+    (mul
+        (sine2 (sum freq (mul freq t (sine (* 4.77 freq)))))
+        (mul 0.4 e))))
+
+(defn demo-afunc [e]
+  (let [melody (ref (take (* 4 8) (cycle [220 330 440 330])))
+        dur 0.4
+        cur-time (double-array 1 0.0)
+        time-incr (/ eng/*ksmps* 44100.0)
+        afs (e :pending-funcs)
+        out (create-buffer)]
+    (dosync (alter afs conj (fm-synth 440)))
+    (fn ^doubles []
+      (let [t (+ (getd cur-time) time-incr)]
+        (when (> t dur)
+          (dosync (alter afs conj (fm-bell 220))))
+        (setd! cur-time (rem t dur)))
+      out
+      )))
+
+
+;;
+
+(comment
+
+  (defn note-sender[e]
+    (dosync
+      (alter (e :pending-funcs) conj (fm-synth 440) (fm-synth 660))))
+
+  (def e (eng/engine-create))
+  (eng/engine-start e)
+  (eng/engine-add-afunc e (demo-afunc e))
+  (eng/engine-stop e)
+
+  (eng/engine-clear e)
+  e
+
+  (let [e (eng/engine-create)]
+    (eng/engine-start e)
+    (demo e)
+    (Thread/sleep 500)
+    (eng/engine-stop e)))
+
+
--- src/audio_seq/envelopes.clj
+++ src/audio_seq/envelopes.clj
@@ -27,10 +27,11 @@
 ;;(defn- not-env-complete
 
 (defn env [pts]
+  "Generates an envelop given time-tagged pairs of values (t0, v0, t1, v1 ...)"
  {:pre (even? (count pts))}
   (let [linedata (make-env-data pts)
         line-samples (reduce + (map first linedata))
-        cur-val (double-array 1 (nth pts 0))
+        cur-val (double-array 1 (nth pts 1))
         counter (long-array 1 -1)
         out (create-buffer)]
  ;; (clojure.pprint/pprint linedata)
@@ -42,3 +43,52 @@
         #(if-let [x (env-get-inc linedata (swapl! counter inc))]
           (+ ^double % x)
           0.0))))))
+
+
+;; EXPONENTIAL ENVELOPE
+
+
+(defn- make-exp-env-data [pts]
+  {:pre (even? (count pts))}
+  (let [[x & xs] (partition 2 pts)]
+    (second (reduce (fn [[[a b] lst] [c d :as p]] 
+                      (let [cnt (double (* c *sr*))
+                            mlt (double (Math/pow (/ d b) (/ 1.0 cnt)))] 
+                        [p (conj lst [b mlt cnt])] ))
+                    [x []] xs))))
+
+
+
+(defn- exp-env-get-inc [data counter]
+  (loop [cnt 0.0 [x & xs] data]
+    (when x
+      (let [[value mlt dur] x
+            c (+ cnt dur)]
+        (if (< counter c)
+          mlt
+          (recur c xs))))))
+
+(defn exp-env [pts]
+ {:pre (even? (count pts))}
+  (let [linedata (make-exp-env-data pts)
+        line-samples (reduce + (map #(nth % 2) linedata))
+        cur-val (double-array 1 (nth pts 1))
+        counter (long-array 1 -1)
+        out (create-buffer)]
+ ;; (clojure.pprint/pprint linedata)
+  ;;(println "Samples: " line-samples)
+  (fn ^doubles[]
+;;    (println "Slope: " (env-get-inc linedata (getl counter)))
+    (when (<= (getl counter) line-samples)
+      (fill out cur-val 
+        #(if-let [x (exp-env-get-inc linedata (swapl! counter inc))]
+          (* ^double % x)
+          0.0))))))
+
+(def pts [0.0 0.001 0.05 1.0 0.3 0.001])
+(def t (exp-env pts))
+(def pts-data (make-exp-env-data pts))
+(def pts-data2 (make-env-data pts))
+(map #(print %) pts-data)
+(map #(print %) pts-data2)
+(t)
