f0803e039f86d78eaee0e6991fca5281f8cfd523
Steven Yi <stevenyi@gmail.com>
2014-09-05 12:01:31 +0100

    updated jvm settings

--- project.clj
+++ project.clj
@@ -1,17 +1,15 @@
 (defproject kunstmusik/pink "0.1.0-SNAPSHOT"
   :jvm-opts ;["-server" "-Xmx2g" "-XX:-UseParallelGC"]
-   ["-Xms512m" "-Xmx1g"           ; Minimum and maximum sizes of the heap
-    "-XX:+UseParNewGC"            ; Use the new parallel GC in conjunction with
-    "-XX:+UseConcMarkSweepGC"     ;  the concurrent garbage collector
-    "-XX:+CMSConcurrentMTEnabled" ; Enable multi-threaded concurrent gc work (ParNewGC)
-    "-XX:MaxGCPauseMillis=20"     ; Specify a target of 20ms for max gc pauses
-    "-XX:+CMSIncrementalMode"     ; Do many small GC cycles to minimize pauses
-    "-XX:MaxNewSize=257m"         ; Specify the max and min size of the new
-    "-XX:NewSize=256m"            ;  generation to be small
-    "-XX:+UseTLAB"                ; Uses thread-local object allocation blocks. This
-                                  ;  improves concurrency by reducing contention on
-                                  ;  the shared heap lock.
-    "-XX:MaxTenuringThreshold=0"] ; Makes the full NewSize available to every NewGC
+  ["-server"
+   "-Xms256m" "-Xmx1g"           ; Minimum and maximum sizes of the heap
+   "-XX:+UseG1GC"
+   "-XX:MaxGCPauseMillis=10"     ; Specify a target of 20ms for max gc pauses
+   "-XX:MaxNewSize=257m"         ; Specify the max and min size of the new
+   "-XX:NewSize=256m"            ;  generation to be small
+   "-XX:+UseTLAB"                ; Uses thread-local object allocation blocks. This
+   ;  improves concurrency by reducing contention on
+   ;  the shared heap lock.
+   "-XX:MaxTenuringThreshold=0" ; Makes the full NewSize available to every NewGC 
   :description "A library for music research, composition, and performance."
 
   :url "http://github.com/kunstmusik/pink"
