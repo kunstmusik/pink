41e74c9a8bcb439eaa20397687f590e95435dac7
Steven Yi <stevenyi@gmail.com>
2014-09-17 08:36:04 +0300

    initial work on audio file loading into tables

---
+++ src/pink/demo/demo_audio_file_load.clj
@@ -0,0 +1,51 @@
+(ns pink.demo.demo-audio-file-load
+ (:require [pink.engine :refer :all]
+             [pink.event :refer :all] 
+             [pink.space :refer [pan]] 
+             [pink.io.sound-file :refer [load-table]]
+             [pink.oscillators :refer [oscil3]]
+             [pink.envelopes :refer [env]]
+             [pink.util :refer [mul]]
+             [pink.node :refer :all]))
+
+(def wave 
+  (load-table "/Users/stevenyi/work/csound/samples/akwf/AKWF_bw_sawbright/AKWF_bsaw_0001.wav"))
+
+(defn instr-waveform
+  [amp freq loc]
+  (pan 
+    (mul (env [0 0 0.02 0.25 0.3 0.25 0.05 0.0])
+         (oscil3 amp freq (:data wave)))
+    loc))
+
+
+(comment
+
+  (def e (engine-create :nchnls 2))
+  (engine-start e)
+
+  (def root-node (create-node :channels 2))
+  (engine-add-afunc e (node-processor root-node))
+
+  (def my-score 
+    (let [num-notes 10] 
+      (node-events root-node 
+                   (map #(event instr-waveform (* % 0.5)  
+                                (/ 0.75 (+ 1 %)) 
+                                (* 220 (+ 1 %)) 
+                                (- (* 2 (/ % (- num-notes 1)))  1)) 
+                        (range num-notes)))))
+
+  (engine-add-events e my-score) 
+
+  (node-add-afunc
+    root-node 
+    (instr-waveform 0.25 (env [0.0 220 0.1 4000 0.0001 220 0.1 4000]) 0.0))
+
+  (engine-stop e)
+  (engine-clear e)
+  (engine-kill-all)
+
+
+  )
+
---
+++ src/pink/io/sound_file.clj
@@ -0,0 +1,102 @@
+(ns pink.io.sound-file
+  (:require [pink.config :refer :all]
+            [pink.util :refer [create-buffers]])
+  (:import [java.io File ByteArrayOutputStream]
+           [java.nio ByteBuffer]
+           [javax.sound.sampled AudioFileFormat$Type AudioFormat
+            AudioFormat$Encoding AudioInputStream AudioSystem]))
+
+(defn- locate-file
+  [f]
+  (cond 
+    (instance? File f) f
+    (string? f) (File. ^String f)))
+
+;(defn- get-target-format
+;  [^AudioFormat src-format]
+;  (AudioFormat. AudioFormat$Encoding/PCM_FLOAT
+;                *sr* 
+;                32 
+;                (.getChannels src-format) 
+;                (* 4 (.getChannels src-format)) ; frame size
+;                *sr* 
+;                true))
+
+;(defn get-audio-data
+;  "Converts to double[][]"
+;  [^AudioInputStream astream]
+;  )
+
+;(defn sfload
+;  "Load given file or filename as sampled audio.  Returns a map with meta-information as
+;  well as audio split into discrete channels. Converts to doubles from source format."
+;  [f]
+;  (let [af ^File (locate-file f)
+;        ain ^AudioInputStream (AudioSystem/getAudioInputStream af)
+;        src-format ^AudioFormat (.getFormat ain)
+;        target-format ^AudioFormat (get-target-format src-format)
+;        astream ^AudioInputStream (AudioSystem/getAudioInputStream 
+;                                    target-format ain) ]
+;    { :channels (.getChannels target-format)
+;     :format target-format 
+;     :data (get-audio-data astream) 
+;     })
+;  )
+
+(defn- convert-to-double-arrays
+  "Convert 16-bit, big endian audio samples to +-1.0 double samples"
+  [^ByteArrayOutputStream baos channels]
+  (let [barray ^bytes (.toByteArray baos)
+        chan-length (/ (alength barray) (* channels 2)) ; assumes 16-bit/2-byte per sample
+        bbuffer (ByteBuffer/wrap barray)
+        out (if (= channels 1) 
+              (double-array chan-length)
+              (into-array (take channels (repeatedly (double-array chan-length)))))]
+    (loop [i 0]
+      (if (< i chan-length)
+        (do
+          (if (= channels 1) 
+            (aset ^doubles out i (double (/ (.getShort bbuffer) 32768.0)))
+            (loop [j 0]
+              (if (< j channels)
+                (aset ^doubles (aget ^"[[D" out j) i (double (/ (.getShort bbuffer) 32768.0)))
+                (recur (unchecked-inc j))) ))
+          (recur (unchecked-inc i))) 
+        out))))
+
+(defn load-table
+  "Load given file or filename as sampled audio.  Returns a map with meta-information as
+  well as audio split into discrete channels. Converts to doubles from source format. 
+  Currently only works with 16-bit PCM_SIGNED wave files."
+  [f]
+  (let [af ^File (locate-file f)
+        ain0 ^AudioInputStream (AudioSystem/getAudioInputStream af)
+        src-format ^AudioFormat (.getFormat ain0)
+        ain ^AudioInputStream
+        (AudioSystem/getAudioInputStream
+          (AudioFormat. AudioFormat$Encoding/PCM_SIGNED
+                        (.getSampleRate src-format) 
+                        (.getSampleSizeInBits src-format) 
+                        (.getChannels src-format) 
+                        (.getFrameSize src-format)
+                        (.getSampleRate src-format) 
+                        true)
+          ain0) 
+        channels (.getChannels src-format)
+        baos (ByteArrayOutputStream.)
+        buffer (byte-array 4096)]
+    (loop [cnt (.read ain buffer)]
+      (if (> cnt 0)
+        (do 
+          (.write baos buffer 0 cnt)
+          (recur (.read ain buffer)))
+        { :channels channels 
+         ;:format target-format 
+         :data (convert-to-double-arrays baos channels) 
+         }))))
+
+;(def a (load-table "/Users/stevenyi/work/csound/samples/akwf/AKWF_bw_sawbright/AKWF_bsaw_0001.wav"))
+
+;(require '[clojure.pprint :refer [pprint]])
+;(println (alength (:data a)))
+;(pprint a)
