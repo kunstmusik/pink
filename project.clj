0266699b621146ff7b11826b4e0f6328b340ccaf
Steven Yi <stevenyi@gmail.com>
2014-10-30 11:40:06 -0400

    continuing tuning of jvm settings

--- project.clj
+++ project.clj
@@ -2,15 +2,15 @@
   :jvm-opts ;["-server" "-Xmx2g" "-XX:-UseParallelGC"]
   ^:replace
   ["-server"
-   "-Xms256m" "-Xmx512m"           ; Minimum and maximum sizes of the heap
+   "-Xmx512m"           ; Minimum and maximum sizes of the heap
    "-XX:+UseG1GC"
-   "-XX:MaxGCPauseMillis=10"     ; Specify a target of 20ms for max gc pauses
-   "-XX:MaxNewSize=257m"         ; Specify the max and min size of the new
-   "-XX:NewSize=256m"            ;  generation to be small
+   "-XX:MaxGCPauseMillis=1"     ; Specify a target of 20ms for max gc pauses
+   ;"-XX:MaxNewSize=257m"         ; Specify the max and min size of the new
+   ;"-XX:NewSize=256m"            ;  generation to be small
    "-XX:+UseTLAB"                ; Uses thread-local object allocation blocks. This
    ;  improves concurrency by reducing contention on
    ;  the shared heap lock.
-   "-XX:MaxTenuringThreshold=0" ; Makes the full NewSize available to every NewGC 
+   ;"-XX:MaxTenuringThreshold=0" ; Makes the full NewSize available to every NewGC 
 
    ; for GC diagnostics 
    ;"-XX:+PrintGCDetails"
