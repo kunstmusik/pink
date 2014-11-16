f0ab17baa319230691388ff524aabf9463eaaa64
Steven Yi <stevenyi@gmail.com>
2014-11-16 13:45:26 -0500

    fixed add-events and add-audio-events to work with var-arg style event writing; added i function for creating Csound style events (afunc, start, dur, & args), added with-afunc to simplify notelist writing

--- src/pink/simple.clj
+++ src/pink/simple.clj
@@ -2,7 +2,10 @@
   ^{:doc "Simple interface for single-engine projects"
    :author "Steven Yi"}
   pink.simple
-  (:require [pink.engine :refer :all]))
+  (:require [pink.engine :refer :all]
+            [pink.util :refer [with-duration]]
+            [pink.event :refer [event]]
+            ))
 
 (def engine (engine-create :nchnls 2))
 
@@ -35,10 +38,41 @@
 
 (defn add-events 
   "Takes in list of events and adds to engine's event list."
-  [evts]
+  ([evts]
   (engine-add-events engine evts))
+  ([evt & evts]
+   (add-events (list* evt evts))))
 
 (defn add-audio-events 
   "Takes in list of events, wraps in audio events, and adds to engine's event list."
-  [evts]
+  ([evts]
   (engine-add-events engine (audio-events engine evts)))
+  ([evt & evts]
+   (add-audio-events (list* evt evts))))
+
+
+;; higher level
+
+(defn apply-afunc-with-dur
+  "Applies an afunc to args, wrapping results with (with-duration dur)."
+  [afunc dur & args]  
+  (with-duration dur
+    (apply afunc args)))
+
+(defn i
+  "Csound style note events: audio-func, start, dur, & args. 
+  Wraps into an event that will call audio-func with args, and wrap 
+  with with-duration call with dur. Most likely used in conjunction
+  with add-audio-events so that generated afuncs will be added to 
+  to an engine."
+  [afunc start dur & args]
+  (apply event apply-afunc-with-dur start afunc dur args))
+
+(defn with-afunc
+  "Wraps note lists with calls to i with audio-func to use."
+  ([afunc notelist]
+   (map #(apply i afunc %) notelist))
+  ([afunc note & notes]
+  (with-afunc afunc (list* note notes))))
+
+
