cf73a486b39d344f21b83d3dd9e2c500ce84b5eb
Steven Yi <stevenyi@gmail.com>
2014-11-25 11:09:15 -0500

    added ns doc

--- src/pink/config.clj
+++ src/pink/config.clj
@@ -1,4 +1,7 @@
-(ns pink.config)
+(ns 
+  ^{:doc "Dynamically-scoped variables used to alter the context of processing."
+   :author "Steven Yi"}
+  pink.config)
 
 (def ^:dynamic *sr* 44100)
 (def ^:dynamic *buffer-size* 64)
