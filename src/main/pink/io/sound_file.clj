a0997fdb87e6519235ec50ebd29b7b6beee8c841
Steven Yi <stevenyi@gmail.com>
2016-05-17 13:18:32 -0400

    initial work on WAV file streaming writing code

--- src/main/pink/io/sound_file.clj
+++ src/main/pink/io/sound_file.clj
@@ -1,7 +1,7 @@
 (ns pink.io.sound-file
   (:require [pink.config :refer :all]
             [pink.util :refer [create-buffers]])
-  (:import [java.io File ByteArrayOutputStream]
+  (:import [java.io File ByteArrayOutputStream FileOutputStream]
            [java.nio ByteBuffer]
            [javax.sound.sampled AudioFormat
             AudioFormat$Encoding AudioInputStream AudioSystem]))
@@ -101,3 +101,78 @@
 ;(require '[clojure.pprint :refer [pprint]])
 ;(println (alength (:data a)))
 ;(pprint a)
+
+(defn- write-wav-header!
+  "Writes WAV file header.  Uses mock values for file and data chunk length
+  that will be re-written once the WAV writing is complete and the file
+  closed."
+  [wav-data ^long sr ^long bit-rate ^long channels] 
+  (let [baos (:byte-array wav-data)
+        fos (:byte-array wav-data) 
+        byte-rate (long (/ bit-rate 8))
+        block-align (* channels byte-rate)
+
+        ]
+
+    ;;; RIFF identifier 
+    ;(.write baos (.toByteArray "RIFF"))
+
+    ;;; file length 
+    ;view.setUint32(4, 32 + samples.length * 2, true);
+
+    ;;; RIFF type 
+    ;(.write baos (.toByteArray "WAVE"))
+
+    ;;; format chunk identifier 
+    ;(.write baos (.toByteArray "fmt "))
+
+    ;;; format chunk length 
+    ;view.setUint32(16, 16, true);
+    ;;; sample format (raw) 
+    ;view.setUint16(20, 1, true);
+    ;;; channel count 
+    ;view.setUint16(22, 1, true);
+    ;;; sample rate 
+    ;view.setUint32(24, sampleRate, true);
+    ;;; byte rate (sample rate * block align) 
+    ;view.setUint32(28, sampleRate * 2, true);
+    ;;; block align (channel count * bytes per sample) 
+    ;view.setUint16(32, 2, true);
+    ;;; bits per sample 
+    ;view.setUint16(34, 16, true);
+    ;;; data chunk identifier 
+    ;(.write baos (.toByteArray "data"))
+    ;;; data chunk length 
+    ;view.setUint32(40, samples.length * 2, true);
+
+    )
+
+  )
+
+(defn open-wave-write
+  "Opens a WAV file for streaming writes. WAV will have a mock header written
+  that will have full information written when close-wav-data is called."
+  [filename sr bit-rate channels]
+
+  (let [baos (ByteArrayOutputStream.)
+        fos (FileOutputStream. (File. ^String filename))
+        wav-data 
+        {:byte-array baos
+         :fos fos
+         :samples-written (atom 0)}
+        ]  
+    (write-wav-header! wav-data sr bit-rate channels)
+    )
+  )
+
+(defn write-wav-data 
+  "Appends new audio data to WAV file."
+  [wav-data interleaved-audio] 
+  
+  )
+
+(defn close-wav-data
+  "Rewrites WAV file header with appropriate values for samples written."
+  [wav-data]
+  
+  )
