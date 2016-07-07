5b8b12f9b4c91bbbea92ccfa2b8177915c76d0da
Steven Yi <stevenyi@gmail.com>
2016-07-07 19:36:47 -0400

    continuing to develop karplus-strong functions

--- src/demo/pink/demo/pluck.clj
+++ src/demo/pink/demo/pluck.clj
@@ -2,14 +2,13 @@
   (:require [pink.simple :refer :all]
              [pink.event :refer :all] 
              [pink.space :refer [pan]] 
-             [pink.filter]
              [pink.instruments.pluck :refer :all] 
              ))
 
 (defn panks
   [& args]
   (->
-    (apply karplus args)
+    (apply plucked args)
     (pan 0.1)))
 
 (comment
@@ -17,13 +16,17 @@
   (start-engine)
   
   (add-audio-events
-    (i panks 0 0.5 0.25 220 6.0))
+    (i panks 0 0.5 0.25 200))
 
   (add-audio-events
-    (i panks 0 0.5 0.25 220 3.0)
-    (i panks 3.0 0.5 0.25 330 3.0)
-    (i panks 5.0 0.5 0.25 440 3.0)
-    (i panks 5.2 0.5 0.25 400 3.0))
+    (i panks 0 0.5 0.25 200)
+    (i panks 0 0.5 0.5 600))
+
+  (add-audio-events
+    (i panks 0 0.5 0.25 220)
+    (i panks 3.0 0.5 0.25 330)
+    (i panks 5.0 0.5 0.25 440)
+    (i panks 5.2 0.5 0.25 400))
 
   (clear-engine)
 
--- src/main/pink/instruments/pluck.clj
+++ src/main/pink/instruments/pluck.clj
@@ -9,46 +9,81 @@
             [pink.filters :refer [tone atone]]
             [pink.delays :refer :all]
             )
-  (:import [clojure.lang IFn$LD]))
+  (:import [clojure.lang IFn$LD IFn$DD]))
+
+(defn ss-one-pole
+  [^double pole ^double gain]
+  (let [b0 (if (> pole 0.0) (- 1.0 pole) (+ 1.0 pole))
+        a1 (- pole)
+        ^doubles last-v (double-array 1 0.0)
+        ]
+    (fn ^double [^double samp]
+      (let [v (- (* b0 (* samp gain))
+                 (* a1 (aget last-v 0)))]
+        (aset last-v 0 v)
+        v))))
+
+(defn- ss-one-zero
+  [^double zero]
+  (let [b0 (if (> zero 0.0) 
+             (/ 1.0 (+ 1.0 zero))
+             (/ 1.0 (- 1.0 zero)))
+        b1 (* (- zero) b0)
+        ^doubles last-v (double-array 1 0.0)]
+    (fn ^double [^double samp]
+      (let [v (+ (* b1 (aget last-v 0))
+                 (* b0 samp))]
+        (aset last-v 0 samp)
+        v))))
 
 (defn- create-noise-buffer
-  ^doubles [amp len]
-  (let [^doubles b (create-delay (* 2 len))
-        l (alength b) ]
+  ^doubles [^double amp len]
+  (let [^doubles b (create-delay len)
+        l (alength b) 
+        pick-filter (ss-one-pole
+                      (- 0.999 (* amp 0.15))
+                      (* 0.5 amp))]
     (loop [indx 0]
       (when (< indx l)
-        (aset b indx (* amp (Math/random)))
+        (aset b indx ^double (pick-filter (Math/random)))
         (recur (inc indx))))
     b))
 
-(defn karplus 
-  "Basic Karplus-Strong implementation.
+;; TODO - add phase delay calculation to delay-line length
+
+(defn plucked 
+  "Basic Karplus-Strong implementation based on Plucked
+  class from STK.
  
-  amp - overall amplitude of pluck
-  freq - frequency in hertz 
-  tau - time for decay of string in seconds
+  amp - overall amplitude of pluck 
+  freq - frequency in hertz
   "
-  [^double amp ^double freq ^double tau]
+  [^double amp ^double freq]
   (let [out (create-buffer)
-        ^doubles delay-buffer (create-noise-buffer amp (/ 1.0 freq))
-        delay-time (/ 2.0 freq)
+        delay-time (/ 1.0 freq)
+        ^doubles delay-buffer 
+        (create-noise-buffer amp delay-time)
         delay-length (alength delay-buffer)
-        end-samp (long (+ 1.0 (* tau *sr*))) 
         rw-ptr (int-array 1 0)
-        pole (tau2pole (/ tau delay-length 4))
         ^IFn$LD del-read (delay-readi delay-buffer delay-time)
-        ]
+        init-loop-gain (Math/min 
+                         0.99999 
+                         (+ 0.995 (* freq 0.000005))) 
+        done-gain (- 1.0 amp)
+        ^IFn$DD loop-filter (ss-one-zero -1.0)]
     (generator
       [rw-ptr (long 0)
-       counter (long 0)]
+       counter (long 0)
+       loop-gain init-loop-gain]
       []
-      (if (and (= 0 int-indx) (>= counter end-samp)) 
-        nil 
-        (let [v (* (.invokePrim del-read rw-ptr) pole)]
-          (aset delay-buffer rw-ptr v)
-          (aset out int-indx v)
-          (gen-recur (rem (unchecked-inc rw-ptr) delay-length)
-                     (unchecked-inc counter))))
+      (let [new-v (.invokePrim del-read rw-ptr)
+            v (.invokePrim loop-filter (* new-v loop-gain))]
+        (aset delay-buffer rw-ptr v)
+        (aset out int-indx (* 3.0 v))
+        (gen-recur 
+          (rem (unchecked-inc rw-ptr) delay-length)
+          (unchecked-inc counter)
+          loop-gain))
       (yield out))))
 
 
